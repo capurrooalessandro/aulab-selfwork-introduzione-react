1e1a981a4fb15857e2e3cdf2c1e4d490bcc037cd
Redemption882 <loquendohyper@gmail.com>
2025-10-21 04:29:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+	return (
+		<>
+			<h1 className="main-title">Selfwork Introduzione React</h1>
+			<label htmlFor="inputId" className="form-label">Label</label>
+			<input type="text" className="form-control" id="inputId" placeholder="placeholder" />
+		</>
+	)
+}
